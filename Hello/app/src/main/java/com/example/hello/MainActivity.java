1f18a67584e7702c6eb8065612796e7d1e1d9fcf
DoYoon <dyk6208@gmail.com>
2021-08-18 20:12:26 +0900

    second commit

--- Hello/app/src/main/java/com/example/hello/MainActivity.java
+++ Hello/app/src/main/java/com/example/hello/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.hello;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //코드 추가
+        Button button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "Button is clicked",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void onButton1Clicked(View v){
+        Toast.makeText(this, "Button is clicked", Toast.LENGTH_LONG).show();
     }
 }
---
+++ Study04/app/src/main/java/org/techtown/study04/MainActivity.java
@@ -0,0 +1,24 @@
+package org.techtown.study04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main2);//버튼눌렀을때 다른 화면으로 전환
+            }
+        });
+    }
+}
